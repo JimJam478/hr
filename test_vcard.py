fe4f5505eacc4a232554de36ffcc377956e68096
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-14 22:34:56 +0530

    Optimizes test for generate_vcard function

--- test_vcard.py
+++ test_vcard.py
@@ -1,6 +1,7 @@
 import gen_vcard
 import os
 
+
 def test_get_data():
     test = "/tmp/sample"
     with open(test, 'w') as f:
@@ -10,6 +11,8 @@ def test_get_data():
     os.unlink(test)
 
 def test_generate_vcs():
+    if not os.path.exists('vcards'):
+        os.mkdir('vcards')
     data = [['Alice','Bob','Software Engineer','alice@example.com','555-555-5555']]
     gen_vcard.generate_vcs(data)
     with open('vcards/alice_bob.vcf', 'r') as f:
@@ -29,3 +32,4 @@ REV:20150922T195243Z
 END:VCARD
 """ in vcard
     os.unlink('vcards/alice_bob.vcf')
+    
