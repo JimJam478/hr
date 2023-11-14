8a45a8cda043c8600d27ee5119b8040a4a2067f4
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-14 16:06:20 +0530

    Splits generate function for better functionality

--- gen_vcard.py
+++ gen_vcard.py
@@ -2,19 +2,22 @@ import csv
 import os
 import sys
 
-def generate_vcard(file_csv):
-    data = []
-    with open(file_csv,'r') as f:
-        reader = csv.reader(f)
-        for item in reader:
-            data.append(item)
-    for item in data:
-            path = r'/home/allen/main_project/vcard'
-            filename = f'{item[0].lower()}_{item[1].lower()}.txt'
-            f = open(os.path.join(path, filename),'w')
-            f.write(f"""BEGIN:VCARD
+def details_from_csv(file_csv):
+  data = []
+  with open(file_csv,'r') as f:
+    reader = csv.reader(f)
+    for item in reader:
+      data.append(item)
+  return data
+
+def generate_vcs(data):
+  for item in data:
+    with open(f'vcards/{item[0]}_{item[1]}.vcf','w') as f:
+        f.write(f"""
+BEGIN:VCARD
 VERSION:2.1
-FN:{item[0]} {item[1]}
+N:{item[0]};{item[1]}
+FN:{item[1]} {item[0]}
 ORG:Authors, Inc.
 TITLE:{item[2]}
 TEL;WORK;VOICE:{item[4]}
@@ -23,11 +26,14 @@ EMAIL;PREF;INTERNET:{item[3]}
 REV:20150922T195243Z
 END:VCARD
 """)
-    return f
-
+        
 def main():
-    file_name = sys.argv[1]
-    generate_vcard(file_name)
-    
+    if not os.path.exists('/vcard'):
+       os.mkdir('vcards')
+       file = sys.argv[1]
+       data = details_from_csv(file)
+       generate_vcs(data)
+       
+
 if __name__ == "__main__":
-    main()
+  main()    
