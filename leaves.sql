98b6ac96275b004e2bf499d24c826cb3e4c022da
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-23 17:34:41 +0530

    Sql query

--- leaves.sql
+++
@@ -1,3 +0,0 @@
-insert into employee_leave(leave_date,reason,employee_id) values ('10-11-2023','Fever',2);
-insert into employee_leave(leave_date,reason,employee_id) values ('11-11-2023','Fever',2);
-insert into employee_leave(leave_date,reason,employee_id) values ('15-11-2023','Fever',3);
