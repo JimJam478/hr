2ffa9db3743735ad6d8c807b1673641bb47cffe7
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-30 14:37:49 +0530

    Initial commit

---
+++ db.py
@@ -0,0 +1,6 @@
+import datetime
+import logging
+from typing import List
+
+from sqlalchemy import String, Integer, Date, create_engine, ForeignKey, UniqueConstraint, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, sessionmaker
